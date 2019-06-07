b087ae33832b210e57c159c409e88d59473bb0c3
Alexander von Below <alexander.von_below@telekom.de>
2019-06-06 17:06:25 -0700

    Updated to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /*
@@ -14,6 +14,10 @@ import PackageDescription
 
 let package = Package(
     name: "cgjprofile",
+    products: [
+        .executable(name: "cgjprofile", targets: ["cgjprofile"]),
+        .library(name: "cgjprofileLib", targets: ["cgjprofileCore"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
