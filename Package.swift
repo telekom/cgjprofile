b104beb9b9b4238e1e3d08c67cc213a254764553
Alexander von Below <alexander.von_below@telekom.de>
2019-10-30 12:51:05 +0100

    Fixed package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-package-manager.git", .branch("master")),
-        .package(url: "https://github.com/below/cgjProfileLib", .from("1.0.0")),
+        .package(url: "https://github.com/below/cgjProfileLib", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
